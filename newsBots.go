662ea4ed50429f08c16f202dd689df108515ec99
bege13mot <bege13mot@gmail.com>
2017-12-11 13:44:32 +0400

    refactor

--- newsBots.go
+++ newsBots.go
@@ -13,12 +13,12 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 )
 
-//Wake up
-func MainHandler(resp http.ResponseWriter, _ *http.Request) {
-	resp.Write([]byte("Hi there! I'm Bot!"))
-}
+//MainHandler does wake up for heroku
+// func MainHandler(resp http.ResponseWriter, _ *http.Request) {
+// 	resp.Write([]byte("Hi there! I'm Bot!"))
+// }
 
-//
+//Greeting does greeting string
 func Greeting() string {
 	first := []string{"Привет", "Доброе утро", "Шалом", "Мир вашему дому"}
 	second := []string{"человеки", "мешки с мясом", "котятки", "кожаные ..."}
@@ -28,20 +28,24 @@ func Greeting() string {
 	return first[r.Intn(4)] + ", " + second[r.Intn(4)] + "!" + "\n\n"
 }
 
+//PostMessages send messages to chats
+func PostMessages(bot *tgbotapi.BotAPI, list []string, text string) {
+	for _, chat := range list {
+		iChat, _ := strconv.ParseInt(chat, 10, 64)
+		msg := tgbotapi.NewMessage(iChat, text)
+		bot.Send(msg)
+	}
+}
+
 func main() {
 	botToken := os.Getenv("TelegramBotToken")
 	consumerKey := os.Getenv("CONSUMER_KEY")
 	accessToken := os.Getenv("ACCESS_TOKEN")
-
-	bot, _ := tgbotapi.NewBotAPI(botToken)
 	myID, _ := strconv.Atoi(os.Getenv("MyID"))
 	list := strings.Split(os.Getenv("Chats"), ",")
 
-	//bot.Debug = false
-
+	bot, _ := tgbotapi.NewBotAPI(botToken)
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
 
 	updates := bot.ListenForWebhook("/" + botToken)
 	// updates, err := bot.GetUpdatesChan(u)
@@ -49,24 +53,19 @@ func main() {
 	// 	log.Printf("Get update error: ", err)
 	// }
 
-	http.HandleFunc("/", MainHandler)
+	//u := tgbotapi.NewUpdate(0)
+	//u.Timeout = 60
+	//http.HandleFunc("/", MainHandler)
 	go http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 
-	// В канал updates будут приходить все новые сообщения.
+	// Receive new updates
 	for update := range updates {
 		if update.Message.From.ID == myID && update.Message.Command() == "" {
-			// Создав структуру - можно её отправить обратно боту
-			log.Printf("Chat ID: ", update.Message.Chat.ID)
-			for _, chat := range list {
-				iChat, _ := strconv.ParseInt(chat, 10, 64)
-				msg := tgbotapi.NewMessage(iChat, update.Message.Text)
-				bot.Send(msg)
-			}
+			log.Printf("Chat ID: %d", update.Message.Chat.ID)
+			PostMessages(bot, list, update.Message.Text)
+
 		} else if update.Message.From.ID == myID && update.Message.Command() == "post" {
-
-			text := Greeting()
 			client := pocket.NewClientWithAccessToken(consumerKey, accessToken, "")
-
 			req := pocket.NewRetrieveRequest().OnlyFavorited()
 			m, err := client.Retrieve(req)
 			if err != nil {
@@ -74,27 +73,25 @@ func main() {
 			}
 
 			if val, ok := m["list"].(map[string]interface{}); ok {
+				text := Greeting()
 
 				for k, v := range val {
 					url := v.(map[string]interface{})["given_url"]
 					title := v.(map[string]interface{})["resolved_title"]
 					text += url.(string) + " - " + title.(string) + "\n"
 
+					//Delete item from Pocket
 					req := new(pocket.ModifyRequest)
 					action := pocket.Action{Kind: pocket.ActionDelete, Params: map[string]string{"item_id": k}}
 					req.AddAction(action)
 					m, err := client.Modify(req)
 					if err != nil {
-						log.Fatal("error in modify: %s", err)
+						log.Printf("error in modify: %s", err)
 					}
 					log.Printf("modify response: %s\n", m)
 				}
 
-				for _, chat := range list {
-					iChat, _ := strconv.ParseInt(chat, 10, 64)
-					msg := tgbotapi.NewMessage(iChat, text)
-					bot.Send(msg)
-				}
+				PostMessages(bot, list, text)
 			}
 		}
 	}
